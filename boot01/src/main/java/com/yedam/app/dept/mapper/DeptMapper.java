94f3b7672ee9f9d960710d918a194bdeb46af707
USER <USER@192.168.219.106>
2024-09-08 17:38:32 +0900

    0908

--- boot01/src/main/java/com/yedam/app/dept/mapper/DeptMapper.java
+++ boot01/src/main/java/com/yedam/app/dept/mapper/DeptMapper.java
@@ -2,6 +2,8 @@ package com.yedam.app.dept.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.yedam.app.dept.service.DeptVO;
 
 public interface DeptMapper {
@@ -9,10 +11,15 @@ public interface DeptMapper {
 	public List<DeptVO> selectDeptAll();
 	
 	// 단건조회
-	
-	// 등록
-	
-	// 수정
-	
-	// 삭제
+		public DeptVO selectDeptInfo(DeptVO deptVO);
+		
+		// 등록
+		public int insertDeptInfo(DeptVO deptVO);
+		
+		// 수정
+		public int updateDeptInfo(@Param("did")int deptId, @Param("dept")DeptVO deptVO);
+		
+		// 삭제
+		public int deleteDeptInfo(int deptId);
 }
+
--- boot01/src/main/java/com/yedam/app/dept/web/DeptController.java
+++ boot01/src/main/java/com/yedam/app/dept/web/DeptController.java
@@ -1,11 +1,15 @@
 package com.yedam.app.dept.web;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yedam.app.dept.service.DeptService;
 import com.yedam.app.dept.service.DeptVO;
@@ -28,14 +32,54 @@ public class DeptController {
 		// classpath:/templates/dept/list.html
 	}
 	// 단건조회
+	@GetMapping("deptInfo") 
+	public String deptInfo(DeptVO deptVO, Model model) {
+		DeptVO findVO = deptService.deptInfo(deptVO);
+		model.addAttribute("dept", findVO);
+		return "dept/info";
+	}
 	
 	// 등록 - 페이지
+	@GetMapping("deptInsert")
+	public String deptInsertForm() {
+		return "dept/insert";
+	}
 	
 	// 등록 - 처리
+	@PostMapping("deptInsert")
+	public String deptInsertProcess(DeptVO deptVO) {
+		int did = deptService.deptInsert(deptVO);
+		
+		String url = null;
+		
+		if(did > -1) {
+			url = "redirect:deptInfo?departmentId="+did;
+		}else{
+			url = "redirect:deptList";
+		}
+		return url;
+	}
 	
 	// 수정 - 페이지
+	@GetMapping("deptUpdate")
+	public String deptUpdateForm(DeptVO deptVO, Model model) {
+		DeptVO findVO = deptService.deptInfo(deptVO);
+		model.addAttribute("dept", findVO);
+		return "dept/update";
+	}
 	
 	// 수정 - 처리 : JSON
+	@PostMapping("deptUpdate")
+	@ResponseBody // AJAX
+	public Map<String, Object> 
+		empUpdateAJAXJSON(@RequestBody DeptVO deptVO){
+		return deptService.deptUpdate(deptVO);
+	}
 	
 	// 삭제 - 처리
+	@GetMapping("deptDelete")
+	public String deptDelete(Integer departmentId) {
+		deptService.deptDelete(departmentId);
+		return "redirect:deptList";
+	}
 }
